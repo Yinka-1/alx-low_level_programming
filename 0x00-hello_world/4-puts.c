3a09251ef66f105049f29ec95d672850dab63730
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-09-09 12:16:49 -0700

    My first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
+ * main - Entry point
  *
- * Return: 0 if exited properly. non-zero otherwise
+ * Description: prints "Programming is like a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+
 	puts("Programming is like building a multilingual puzzle\n");
+
 	return (0);
+
 }
