fda7c5614e8cdae394dac2e369cb0ec774a43bb3
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-09-11 17:42:28 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Printing numbers
+ *
+ * Description: Printing numbers of single digit
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		printf("%d\n", a);
+
+	}
+
+	return (0);
+
+}
