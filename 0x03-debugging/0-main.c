548109104e55387ae73eedec3469410da0de5bc0
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-09-15 12:57:42 -0700

    New

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,14 +4,13 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
+ * @i: a character
  *
  * Return: 0
  */
 
 
-
-int main(void)
-
+void positive_or_negative(int i);
 {
 
 	int i;
