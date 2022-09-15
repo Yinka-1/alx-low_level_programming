c4ddd76c743a34609fe08c3226528607d5c10436
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-09-15 15:46:08 -0700

    Positive & Negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * positive_or_negative - tests function that prints if integer
  * is positive or negative
@@ -9,13 +8,9 @@
  * Return: 0
  */
 
-
 int positive_or_negative(int i)
 {
 	i = 0;
-
 	positive_or_negative(i);
-
 	return (0);
-
 }
