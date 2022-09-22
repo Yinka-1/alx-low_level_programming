35baf32a429fdff6aacd21f1a556aa6010b5d521
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-09-22 14:30:42 -0700

    Capital letters

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 char *string_toupper(char *);
+char *cap_string(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
