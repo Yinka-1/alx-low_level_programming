f54138061e50f12d81c1324b417e786002e80ec1
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-09-28 17:08:40 -0700

    b

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
- int is_prime_number(int n);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
+int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
 int factorial(int n);
