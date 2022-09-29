a40b764ff384cdf5f503f306cf71d6bf7f149f30
Olayinka Samuel <110996084+Yinka-1@users.noreply.github.com>
2022-09-29 01:11:27 +0100

    Update 100-is_palindrome.c
    Checking for palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -20,20 +20,6 @@ int last_index(char *s)
 
 
 /**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
- * Return: 0 or 1
- */
-
-int is_palindrome(char *s)
-{
-	int end = last_index(s);
-
-	return (check(s, 0, end - 1, end % 2));
-}
-
-
-/**
  * check - checker for the palindrome
  * @s: string
  * @start: int moves from right to left
@@ -58,4 +44,17 @@ int check(char *s, int start, int end, int pair)
 
 		return (check(s, start + 1, end - 1, pair));
 
+}
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int end = last_index(s);
+
+	return (check(s, 0, end - 1, end % 2));
 }
