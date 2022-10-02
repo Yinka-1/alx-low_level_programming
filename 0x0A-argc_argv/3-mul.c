cfcf86c59e0634e096bfcfb10052daf073eacc1f
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-10-02 12:52:46 -0700

    Multipling arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,21 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	i = (atoi(argv[1])) * (atoi(argv[2]));
-
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
 
-	else
-	{
-		printf("%d\n", i);
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
