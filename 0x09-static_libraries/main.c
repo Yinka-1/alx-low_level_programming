fa81720c20faf871a1b9a94fa10f06e24e82f1cc
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-10-04 11:49:23 -0700

    Creating a static library

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - prints a string
+  *
+  * Return: Always (0)
+  */
+
+int main(void)
+{
+
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
+	return (0);
+
+}
