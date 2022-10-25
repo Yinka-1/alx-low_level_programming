801e7bdc070be80b679df940c86b37a41b4fb378
Olayinka Samuel <olayinkasamuel253@gmail.com>
2022-10-25 10:25:08 -0700

    A function that initializes a variable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
   * struct dog - A struct containing details of a dog
   * @name: name of dog
@@ -14,5 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
